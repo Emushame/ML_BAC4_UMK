8ae9dbbef8e753cf8b3957d19ce39281c66df125
Edouard Mushame <edouard1mushame@gmail.com>
2025-11-15 10:50:31 +0200

    ajouter le fichier python pour rappel des notions de base

---
+++ python1.py
@@ -0,0 +1,11 @@
+ml = 5
+rx = 2
+os = 8
+
+pourc = ((ml+rx+os)/30)*100
+print("le pourcentage obtenu est de", pourc)
+
+# la notion des listes
+liste = [1, 2, 6, 7, 8, 9]
+liste.append(19)
+liste.remove[0]
