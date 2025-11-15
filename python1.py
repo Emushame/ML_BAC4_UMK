ac9e6afd79f1691b843afbd1f983c31ce42e5823
Edouard Mushame <edouard1mushame@gmail.com>
2025-11-15 14:28:49 +0200

    entrainer le modele de regression lineaire

--- python1.py
+++ python1.py
@@ -1,7 +1,7 @@
-import bac4 as bac4_umk
+import bac4
 import pandas as pd
 
-b = bac4_umk.benefice(150, 100)
+b = bac4.benefice(150, 100)
 print("Le bénéfice est de :", b)
 
 liste = pd.DataFrame(1, index=range(3), columns=range(4))
